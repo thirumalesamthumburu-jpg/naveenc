ee31d3e4a36f10cdbeac5463c3edb93636eef2f3
Thiru <thirumalesamthumburu@gmail.com>
2026-04-15 01:18:22 +0530

    Dharani

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"425cfb54d01a9472b3e81d9e76fd63a4a44cfb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1032409526" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3557050132" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
